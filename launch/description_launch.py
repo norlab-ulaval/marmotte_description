c1bf4c24583066f73f8019cc5b48e9aab3b790c7
norlab-marmotte <norlabmarmotte@gmail.com>
2023-01-18 10:11:22 -0500

    New ROS2 version of marmotte_description with visualization and the arm

--- launch/description_launch.py
+++ launch/description_launch.py
@@ -1,23 +1,38 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, Command
+from launch.substitutions import Command
 from launch_ros.actions import Node
-
+from launch_ros.descriptions import ParameterValue
 
 def generate_launch_description():
 
-    xacro_path = os.path.join(get_package_share_directory("marmotte_description"), "urdf/marmotte.urdf.xacro")
+    params = {
+        "arm": "gen3", #Not use
+        "dof": "7", #Not use
+        "vision": "true", #Not use
+        "sim": "false", #Not use
+        "gripper": "robotiq_2f_85", #Not use
+        "start_rviz": "false" #Not use
+    }
+
+    package = get_package_share_directory("marmotte_description")
+    xacro_path = os.path.join(package, "urdf/marmotte.urdf.xacro")
 
     return LaunchDescription([
-        # DeclareLaunchArgument('xacro_ath', default_value=None, ),
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
-            parameters=[{"robot_description": Command(['xacro', ' ', xacro_path])}]),
+            parameters=[{"robot_description": ParameterValue(Command(["xacro ",xacro_path]), value_type=str)
+                        #  "dof": params["dof"],
+                        #  "vision": params["vision"],
+                        #  "sim": params["sim"]
+                         }]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        )
     ])
